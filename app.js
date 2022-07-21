e181e0e67d44674d950a4d05113630f7feb9b493
Idris Uslu <idrisuslu.007@hotmail.com>
2022-07-21 14:20:28 +0300

    Update...

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
 
-const apiKey = process.env.API_KEY
+// const apiKey = process.env.API_KEY
+const apiKey = b455153d3dd7059af03762bbe91dcf0a;
 const url = "https://api.themoviedb.org/3/movie/";
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,8 +15,18 @@ app.use(express.static("public"));
 
 app.set('view engine','ejs');
 
+// mongoose.connect("")
+
 app.get("/", function(req, res) {
   res.render("home");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
 });
 
 app.post("/", function(req, res) {
@@ -41,6 +53,12 @@ app.post("/", function(req, res) {
       });
     })
   });
+
+});
+
+app.post("/register", function(req, res) {
+
+
 
 });
 
